5edbdce2c74240d93f5e8732ee62e2b9b36437d8
Viktor Jacynycz Garcia <viktor@jacynycz.es>
2017-10-02 16:31:47 +0200

    Refactor & new reviews

--- Contracts/DAJ.sol
+++ Contracts/DAJ.sol
@@ -7,7 +7,7 @@ contract DAJ{
     struct Paper{
         // IPFS Address of the file
         // i.e. QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH
-        string ipfsAddress;
+        string ipfsPaperAddress;
 
         // Ethereum's address of the author
         address[] authors;
@@ -21,39 +21,62 @@ contract DAJ{
         //Reviewers asigned to the Paper
         address[] reviewers;
 
-        Review[] reviews;
-
+        //Hashes of the reviews
+        bytes32[] reviewHashes;
     }
 
     struct Review{
         //Reviews
         address reviewer;
-        string reviewIpfsAddress;
+        string ipfsPaperAddress;
+        string ipfsReviewAddress;
+        /*
+        0 - Reject
+        1 - Major Revision
+        2 - Minor Revision
+        3 - Accept
+        */
         uint acceptance;
     }
 
     struct Rating{
         address rater;
         address reviewer;
+        bytes32 reviewHash;
         uint numRating;
     }
 
+    /**
+    Variables
+    **/
 
-    //Variables
-    mapping(uint => Paper) public papers;
+    //Mapping for editors - level of privilege
+    mapping(address => uint) editors;
+
+    //Mapping structure for papers
+    mapping(uint => Paper) public idPaperMap;
     mapping(string => uint) ipfsPaperMap;
     uint numPapers;
 
+    //Owner of the contract
     address owner;
 
+    //Pending papers waiting for reviews
     uint[] pendingPapers;
 
+    //Reviews mapping of the pending papers
+    mapping(bytes32 => Review) reviewHashMapping;
+
+    //Rating mapping of the reviews
     mapping(bytes32 => Rating) reviewsRating;
 
-    //Modifiers
-    modifier onlyReviewerAssigned(string _ipfsAddress){
+    /*
+    Modifiers
+    */
+    modifier onlyReviewerAssigned(string _ipfsPaperAddress){
         bool canReview = false;
-        address[] storage addresses = papers[ipfsPaperMap[_ipfsAddress]].reviewers;
+        address[] storage addresses = idPaperMap[ipfsPaperMap[_ipfsPaperAddress]].reviewers;
+        //TODO modify num-reviwer
         for(uint i = 0 ; i < 3 ; i++){
             if ( addresses[i] == msg.sender ){
                 canReview = true;
@@ -63,113 +86,155 @@ contract DAJ{
         _;
     }
 
+    //Level 1 needed
+    modifier onlyEditors(){
+        require(editors[msg.sender] > 0);
+        _;
+    }
+
+    //Level 2 needed
+    modifier onlyLevel2Editors(){
+        require(editors[msg.sender] > 1);
+        _;
+    }
+
     //Events
     event ReviewersAssigned(
         address[] _reviewerAddresses,
         uint _paperId,
-        string _ipfsAddress);
+        string _ipfsPaperAddress);
     event PaperSent(
         address _from,
         address[] _authors,
         uint _paperId,
-        string _ipfsAddress);
+        string _ipfsPaperAddress);
+    event PaperPublished(string _ipfsPaperAddress);
     event ReviewSent(
         address _reviewerAddress,
+        string _ipfsPaperAddress,
+        string _ipfsReviewAddress,
         uint _aceptance,
-        string _paperIpfsAddress,
-        string _reviewIpfsAddress);
+        bytes32 reviewHash);
     event ReviewRated(
         address _from,
         address _toReviewer,
-        //string _reviewIpfsAddress,
-        string _paperIpfsAddress,
+        string _ipfsPaperAddress,
+        string _ipfsReviewAddress,
         uint _rating
         );
 
-    //Functions
+    /*
+    Functions
+    */
+
     function DAJ() public{
         numPapers = 0;
         owner = msg.sender;
     }
 
-    function getPaper(uint numPaper)
+    function getPaper(uint paperId)
     constant
     public
     returns (
-        string ipfsAddres,
+        string ipfsPaperAddress,
         string btcAddress,
         address[] authors,
         address[] reviewers
         ){
         return (
-            papers[numPaper].ipfsAddress,
-            papers[numPaper].btcAddress,
-            papers[numPaper].authors,
-            papers[numPaper].reviewers
+            idPaperMap[paperId].ipfsPaperAddress,
+            idPaperMap[paperId].btcAddress,
+            idPaperMap[paperId].authors,
+            idPaperMap[paperId].reviewers
         );
     }
 
     function sendPaper(
-        string _ipfsAddress,
+        string _ipfsPaperAddress,
         string _btcAddress,
         address[] _authors
         )
         public{
-        papers[numPapers].ipfsAddress = _ipfsAddress;
-        papers[numPapers].btcAddress = _btcAddress;
-        papers[numPapers].authors = _authors;
-        ipfsPaperMap[_ipfsAddress] = numPapers;
-        PaperSent(msg.sender,_authors,numPapers,_ipfsAddress);
+        idPaperMap[numPapers].ipfsPaperAddress = _ipfsPaperAddress;
+        idPaperMap[numPapers].btcAddress = _btcAddress;
+        idPaperMap[numPapers].authors = _authors;
+        ipfsPaperMap[_ipfsPaperAddress] = numPapers;
+        PaperSent(msg.sender,_authors,numPapers,_ipfsPaperAddress);
         numPapers++;
     }
 
-    function assignReviewers(uint _paperId, address[] _reviewers)
+    function assignReviewers(string _ipfsPaperAddress, address[] _reviewers)
     public
-    //only editors TODO modifier
+    onlyEditors()
     {
-        papers[_paperId].reviewers = _reviewers;
-        ReviewersAssigned(_reviewers,_paperId,papers[_paperId].ipfsAddress);
+        uint paperId = ipfsPaperMap[_ipfsPaperAddress];
+        idPaperMap[paperId].reviewers = _reviewers;
+        ReviewersAssigned(_reviewers,paperId,idPaperMap[paperId].ipfsPaperAddress);
     }
 
     function sendReview(
-      string _ipfsAddress,
+      string _ipfsPaperAddress,
       uint _acceptance,
-      string _reviewIpfsAddress
+      string _ipfsReviewAddress
       )
     public
-    onlyReviewerAssigned(_ipfsAddress){
-        uint paperId = ipfsPaperMap[_ipfsAddress];
+    onlyReviewerAssigned( _ipfsPaperAddress ){
+        uint paperId = ipfsPaperMap[_ipfsPaperAddress];
         Review memory newReview = Review(
           msg.sender,
-          _reviewIpfsAddress,
+          _ipfsPaperAddress,
+          _ipfsReviewAddress,
           _acceptance
+        );
+        bytes32 reviewHash = keccak256(msg.sender, _ipfsReviewAddress);
+        reviewHashMapping[reviewHash] = newReview;
+        idPaperMap[paperId].reviewHashes.push(reviewHash);
+        ReviewSent(
+          msg.sender,
+          _ipfsPaperAddress,
+          _ipfsReviewAddress,
+          _acceptance,
+          reviewHash
           );
-        papers[paperId].reviews.push(newReview);
-        ReviewSent(msg.sender, _acceptance, _ipfsAddress, _reviewIpfsAddress);
         //TODO check if the paper is accepted
         checkAcceptance(paperId);
     }
 
     function checkAcceptance(uint paperId)
     private{
-        Review[] memory reviews = papers[paperId].reviews;
-        for(uint i = 0 ; i < 3 ; i++){
-            if(reviews[i].acceptance < 2){
+
+        //Custom acceptance metrics
+        bytes32[] memory hashes = idPaperMap[paperId].reviewHashes;
+        for(uint i = 0 ; i < hashes.length ; i++){
+            if(reviewHashMapping[hashes[i]].acceptance < 2){
                 return;
             }
         }
-        papers[paperId].published = true;
+        idPaperMap[paperId].published = true;
+        PaperPublished(idPaperMap[paperId].ipfsPaperAddress);
     }
-    //TODO function to rate reviews
 
-    function rateReview(
-        string _ipfsAddress,
-        address _reviewerAddress,
-        uint _rateReview)
+    function rateReview(bytes32 _reviewHash, uint _rateReview)
     public{
-        bytes32 reviewHash = keccak256(_ipfsAddress, _reviewerAddress);
-        Rating memory newRating = Rating(msg.sender, _reviewerAddress, _rateReview);
-        reviewsRating[reviewHash] = newRating;
-        ReviewRated(msg.sender,_reviewerAddress, _ipfsAddress, _rateReview);
+        Review memory review = reviewHashMapping[_reviewHash];
+        bytes32 rateOfReviewHash = keccak256(
+          review.ipfsPaperAddress,
+          review.reviewer,
+          msg.sender
+        );
+        Rating memory newRating = Rating(
+            msg.sender,
+            review.reviewer,
+            _reviewHash,
+            _rateReview
+            );
+        reviewsRating[rateOfReviewHash] = newRating;
+        ReviewRated(
+            msg.sender,
+            review.reviewer,
+            review.ipfsPaperAddress,
+            review.ipfsReviewAddress,
+            _rateReview
+        );
     }
 }
